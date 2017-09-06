56ce6a55f8ba48b0e9394b4f9506de643711e719
kayibal <alan.f.hoeng@gmail.com>
2017-09-06 18:24:37 +0200

    Final adjustements little bugfixes

--- cluster_test.py
+++ cluster_test.py
@@ -1,40 +1,38 @@
-import pandas as pd
-import numpy as np
-import time
 import logging
+import time
+
 import dask.dataframe as dd
+import numpy as np
+import pandas as pd
 from distributed import Client
 
-
-logging.getLogger(__name__)\
-    .info("Wating for cluster to come online")
-
-time.sleep(30)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-logging.getLogger(__name__)\
-    .info("======= TESTING DASK CLUSTER ========")
+if __name__ == '__main__':
 
-c = Client('dask-scheduler:8786')
+    logger.info("Wating for cluster to come online")
 
-data = np.arange(10000)
+    time.sleep(30)
 
-df = pd.DataFrame(dict(col1=data, col2=data[::-1]))
+    logger.info("======= TESTING DASK CLUSTER ========")
 
-ddf = dd.from_pandas(df, npartitions=10)
+    c = Client('dask-scheduler:8786')
 
-c.scatter(ddf)
+    data = np.arange(10000)
 
-logging.getLogger(__name__)\
-    .info("Submitting job...")
+    df = pd.DataFrame(dict(col1=data, col2=data[::-1]))
 
-res = dd.concat([ddf.sum(), ddf.loc[:500].sum()],
-                interleave_partitions=True).compute()
+    ddf = dd.from_pandas(df, npartitions=10)
 
-logging.getLogger(__name__)\
-    .info("Result:\n{}".format(str(res)))
+    c.scatter(ddf)
 
-logging.getLogger(__name__)\
-    .info("======= CLUSTER OK ========")
+    logger.info("Submitting job...")
 
-print(res)
+    res = dd.concat([ddf.sum(), ddf.loc[:500].sum()],
+                    interleave_partitions=True).compute()
+
+    logger.info("Result:\n{}".format(str(res)))
+
+    logger.info("======= CLUSTER OK ========")
--- dockerfile
+++ dockerfile
@@ -2,3 +2,7 @@ FROM ubuntu:16.04
 ENV LC_ALL=C.UTF-8
 ENV LANG=C.UTF-8
 
+COPY cluster_test.py /root/
+COPY requirements.txt /tmp/requirements.txt
+
+# Provision container
